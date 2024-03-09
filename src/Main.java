9045a8191defa5949fdcab294cfe061c5b490d91
Dimas test <khaerbash@gmail.com>
2024-03-09 17:06:08 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676;
+        int percent = 20;
+        int bonus = amount / percent;
+        System.out.println("Итоговый бонус: " + bonus);
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
